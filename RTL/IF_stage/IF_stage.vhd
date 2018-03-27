a66ae1cbe34cd71c30695cbca9447663dd58805f
Tomislav Bićanić <tb48445@fer.hr>
2018-03-27 17:06:07 +0200

    Added IF_stage.vhd (nothing implemented yet)

---
+++ RTL/IF_stage/IF_stage.vhd
@@ -0,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity IF_stage is
+	port(
+		clk		: in std_logic;
+		rst		: in std_logic
+		
+	);
+	
+end IF_stage;
+
+architecture rtl of IF_stage is
+
+begin
+
+
+end rtl;
+
