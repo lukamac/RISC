2b7c2cdfb3d342415204dbd1c13f5720c89b5dd5
Luka Macan <lm47721@fer.hr>
2018-03-27 17:01:36 +0200

    Start on alu.vhd. Deleted all the .swp files.

--- RTL/ALU/alu.vhd
+++ RTL/ALU/alu.vhd
@@ -11,8 +11,8 @@ entity alu is
     port
     (
         op      : in op_t;
-        reg_a   : in word_t;
-        reg_b   : in word_t;
+        a       : in word_t;
+        b       : in word_t;
         res     : out word_t;
     );
 end entity alu;
@@ -20,19 +20,37 @@ end entity alu;
 architecture rtl of alu is
 
     signal adder_a, adder_b : signed(word_t'range);
+    signal not_b : word_t;
+
+    signal state_reg, state_next : word_t;
 
 begin
 
-    alu_execution: process (op, reg_a, reg_b) is
+    process (clk) is
     begin
-        adder_a <= reg_a;
-        adder_b <= reg_b;
+        if (rising_edge(clk)) then
+            if (rst = '1') then
+                state_reg <= (others => '0');
+            else
+                state_reg <= state_next;
+            end if;
+        end if;
+    end;
+
+    alu_execution: process (op, a, b) is
+    begin
+        adder_a <= a;
+        adder_b <= b;
+        res <= adder;
         case (op) is
             when ADD =>
             when SUB =>
-                adder_b <= (not reg_b + 1);
+                adder_b <= (signed(not_b) + 1);
+            when ADDI =>
         end case;
     end process;
+
+    not_b <= not b;
 
     adder <= adder_a + adder_b;
 
