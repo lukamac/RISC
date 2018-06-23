d290ccea86724ac5d51757fad70a4a443c82c3a5
Luka Macan <luka.macan@fer.hr>
2018-06-23 14:02:54 +0200

    Izbriso fileove od applea

--- RTL/controller/controller.vhd
+++ RTL/controller/controller.vhd
@@ -12,10 +12,15 @@ entity controller is
         rst, clk : in std_logic;
         instr : in word_t;
 
-        
-
+        -- EX stage control signals
         alu_b, alu_c : out std_logic;
-        alu_op : out op_t
+        alu_op : out op_t;
+
+        -- MEM stage control signals
+        mem_en, rw : out std_logic;
+
+        -- WB stage control signals
+        wb_mux : out std_logic
     );
 end entity controller;
 
@@ -47,7 +52,7 @@ begin
     past_instr_next(3 downto 1) <= past_instr_reg(2 downto 0);
     past_instr_next(0) <= instr;
 
-    alu: process (past_instr_reg(EX_stage)) is
+    EX: process (past_instr_reg(EX_stage)) is
         variable EX_instr : word_t := past_instr_reg(EX_stage);
         variable op : op_t := EX_instr(31 downto 27);
     begin
@@ -57,11 +62,38 @@ begin
         alu_c <= '0';
         case op is
             when ADDI_OP | SUBI_OP | ORI_OP   | ANDI_OP |
-                 SHI_OP  | SHLI_OP | SHRAI_OP | SHCI_OP =>
+                 SHRI_OP  | SHLI_OP | SHRAI_OP | SHCI_OP =>
                 alu_c <= '1';
             when others =>
                 null;
         end case;
-    end process alu;
+    end process EX;
+
+
+    MEM: process (past_instr_reg(ME_stage)) is
+        variable MEM_instr : word_t := past_instr_reg(ME_stage);
+        variable op : op_t := MEM_instr(31 downto 27);
+    begin
+        mem_en <= '0';
+        rw <= '0';
+        case op is
+            --TODO When we add memory commands
+            when others =>
+                null;
+        end case;
+    end process MEM;
+
+
+    WB: process (past_instr_reg(WE_stage)) is
+        variable WB_instr : word_t := past_instr_reg(WE_stage);
+        variable op : op_t := WB_instr(31 downto 27);
+    begin
+        wb_mux <= '0';
+        case op is
+            --TODO when we add memory commands
+            when others =>
+                null;
+        end case;
+    end process WB;
 
 end architecture RTL;
